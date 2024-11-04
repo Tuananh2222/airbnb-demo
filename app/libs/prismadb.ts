364a259a7f9c1d9cadf732b76805216c669de21e
Twanhcoder <twanh.dev@gmail.com>
2024-11-04 14:34:23 +0700

    update prisma 2

--- app/libs/prismadb.ts
+++ app/libs/prismadb.ts
@@ -8,6 +8,7 @@ declare global {
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const client = (globalThis as any).prisma || new PrismaClient();
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 if (process.env.NODE_ENV !== "production") (globalThis as any).prisma = client;
 
 export default client;
